38a21e61de3ea00147c5ca84ffa69fa3d06b4e25
KarenKrill <vampart1997@gmail.com>
2025-11-18 23:45:42 +0500

    Added game states and state graph

---
+++ Assets/Madnessnoid/Scripts/GameStates/Abstractions/GameState.cs
@@ -0,0 +1,14 @@
+namespace Madnessnoid.Abstractions
+{
+    public enum GameState
+    {
+        Initial,
+        Loading,
+        MainMenu,
+        Gameplay,
+        LevelWin,
+        LevelLose,
+        Pause,
+        Exit
+    }
+}

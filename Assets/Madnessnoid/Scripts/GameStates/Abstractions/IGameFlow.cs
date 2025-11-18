cd44ae9318d710cc8be3516d3af04f77f32d4023
KarenKrill <vampart1997@gmail.com>
2025-11-18 23:57:34 +0500

    Added GameFlow abstraction

---
+++ Assets/Madnessnoid/Scripts/GameStates/Abstractions/IGameFlow.cs
@@ -0,0 +1,13 @@
+﻿namespace Madnessnoid.Abstractions
+{
+    public interface IGameFlow
+    {
+        GameState State { get; }
+
+        void LoadMainMenu();
+        void StartLevel(int index);
+        void PauseLevel();
+        void ResumeLevel();
+        void Exit();
+    }
+}

5f08f06737db298dab2c7e41630eceee9d231f7b
KarenKrill <vampart1997@gmail.com>
2025-11-24 00:48:57 +0500

    Added missing state handler base

---
+++ Assets/Madnessnoid/Scripts/GameStates/StateHandlers/PresentableStateHandlerBase.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+
+using System;
+using System.Linq;
+
+using KarenKrill.UniCore.StateSystem.Abstractions;
+using KarenKrill.UniCore.UI.Presenters.Abstractions;
+
+namespace Madnessnoid.GameStates
+{
+    public abstract class PresentableStateHandlerBase<T> : IStateHandler<T> where T : Enum
+    {
+        public abstract T State { get; }
+
+        public PresentableStateHandlerBase(params IPresenter[] presenters)
+        {
+            _presenters = presenters.ToArray();
+        }
+        public virtual void Enter(T prevState, object? context = null)
+        {
+            foreach (var presenter in _presenters)
+            {
+                presenter.Enable();
+            }
+        }
+        public virtual void Exit(T nextState)
+        {
+            foreach (var presenter in _presenters)
+            {
+                presenter.Disable();
+            }
+        }
+
+        private readonly IPresenter[] _presenters;
+    }
+}

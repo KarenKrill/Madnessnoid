73264c1fcfe4f8d5d1683b247a2d5ae84a053f32
KarenKrill <vampart1997@gmail.com>
2025-11-24 00:49:41 +0500

    Added pause menu state handler

---
+++ Assets/Madnessnoid/Scripts/GameStates/StateHandlers/PauseStateHandler.cs
@@ -0,0 +1,75 @@
+﻿#nullable enable
+
+using UnityEngine;
+
+using KarenKrill.UniCore.Input.Abstractions;
+using KarenKrill.UniCore.StateSystem.Abstractions;
+
+using Madnessnoid.Abstractions;
+using Madnessnoid.Input.Abstractions;
+using Madnessnoid.UI.Presenters.Abstractions;
+
+namespace Madnessnoid.GameStates
+{
+    public class PauseStateHandler : PresentableStateHandlerBase<GameState>, IStateHandler<GameState>
+    {
+        public override GameState State => GameState.Pause;
+
+        public PauseStateHandler(ILogger logger,
+            IGameFlow gameFlow,
+            IStateSwitcher<GameState> stateSwitcher,
+            IBasicActionsProvider actionsProvider,
+            IUIActionsProvider uiActionsProvider,
+            IPauseMenuPresenter pauseMenuPresenter)
+            : base(pauseMenuPresenter)
+        {
+            _logger = logger;
+            _gameFlow = gameFlow;
+            _stateSwitcher = stateSwitcher;
+            _actionsProvider = actionsProvider;
+            _uiActionsProvider = uiActionsProvider;
+            _pauseMenuPresenter = pauseMenuPresenter;
+
+        }
+        public override void Enter(GameState prevState, object? context = null)
+        {
+            base.Enter(prevState);
+            _previousState = prevState;
+            _previousTimeScale = Time.timeScale;
+            Time.timeScale = 0;
+            _logger.Log(nameof(PauseStateHandler), nameof(Enter));
+            _uiActionsProvider.Cancel += OnResumeRequested;
+            _pauseMenuPresenter.Resume += OnResumeRequested;
+            _pauseMenuPresenter.MainMenu += OnMainMenu;
+            _pauseMenuPresenter.Exit += OnExit;
+            _actionsProvider.SetActionMap(ActionMap.UI);
+        }
+        public override void Exit(GameState nextState)
+        {
+            _logger.Log(nameof(PauseStateHandler), nameof(Exit));
+            _uiActionsProvider.Cancel -= OnResumeRequested;
+            _pauseMenuPresenter.Resume -= OnResumeRequested;
+            _pauseMenuPresenter.MainMenu -= OnMainMenu;
+            _pauseMenuPresenter.Exit -= OnExit;
+            Time.timeScale = _previousTimeScale;
+            base.Exit(nextState);
+        }
+
+        private readonly ILogger _logger;
+        private readonly IGameFlow _gameFlow;
+        private readonly IStateSwitcher<GameState> _stateSwitcher;
+        private GameState _previousState;
+        private readonly IBasicActionsProvider _actionsProvider;
+        private readonly IUIActionsProvider _uiActionsProvider;
+        private readonly IPauseMenuPresenter _pauseMenuPresenter;
+        private float _previousTimeScale;
+
+        private void OnResumeRequested()
+        {
+            _stateSwitcher.TransitTo(_previousState);
+        }
+        private void OnMainMenu() => _gameFlow.LoadMainMenu();
+        private void OnExit() => _gameFlow.Exit();
+
+    }
+}

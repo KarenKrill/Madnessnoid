45abfb6b882a110e09eb0c71e742501e44cfc84d
KarenKrill <vampart1997@gmail.com>
2025-11-28 03:07:37 +0500

    Fixed paddle mobile control + optimized AutoLetterbox usage

--- Assets/Madnessnoid/Scripts/Gameplay/PaddleMovement.cs
+++ Assets/Madnessnoid/Scripts/Gameplay/PaddleMovement.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Threading;
-
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 using Cysharp.Threading.Tasks;
 using Zenject;
@@ -23,6 +24,8 @@ namespace Madnessnoid
         [SerializeField]
         private Transform _paddleTransform;
         [SerializeField]
+        private SpriteRenderer _paddleRenderer;
+        [SerializeField]
         private BallController _ballController;
         [SerializeField, Min(0)]
         private float _moveSpeed = 5;
@@ -32,24 +35,44 @@ namespace Madnessnoid
         private float _minX = -5;
         [SerializeField]
         private float _maxX = 5;
+        [SerializeField]
+        private Camera _camera;
 
         private ILogger _logger;
         private IPlayerActionsProvider _playerActionsProvider;
-        private readonly InputAction _touch0PressAction = new(type: InputActionType.Button, binding: "<Touchscreen>/touch0/press");
+        private readonly InputAction _touchPosAction = new(type: InputActionType.Value, binding: "<Touchscreen>/primarytouch/position");
         private readonly InputAction _touch0PosAction = new(type: InputActionType.Value, binding: "<Touchscreen>/touch0/position");
+        private readonly InputAction _touch1PosAction = new(type: InputActionType.Value, binding: "<Touchscreen>/touch1/position");
+        private readonly InputAction _touch2PosAction = new(type: InputActionType.Value, binding: "<Touchscreen>/touch2/position");
+        private readonly InputAction _touch3PosAction = new(type: InputActionType.Value, binding: "<Touchscreen>/touch3/position");
+        private readonly InputAction _touch4PosAction = new(type: InputActionType.Value, binding: "<Touchscreen>/touch4/position");
         private CancellationTokenSource _moveCts;
+        private float _touchPosition = 0;
+        private bool _isLeftTouchDirection = false;
         private float _touchDirection = 0;
+        private Vector2 _paddleSize;
 
-
+        private void Awake()
+        {
+            _paddleSize = GetRendererWorldSize(_paddleRenderer);
+        }
         private void OnEnable()
         {
             if (Application.isMobilePlatform)
             {
-                _touch0PressAction.started += OnTouch0PressStarted;
-                _touch0PressAction.canceled += OnTouch0PressCanceled;
-                _touch0PosAction.performed += OnTouch0PosActionPerformed;
+                _touchPosAction.performed += OnTouchPosActionPerformed;
+                _touch0PosAction.performed += OnTouchPosActionPerformed;
+                _touch1PosAction.performed += OnTouchPosActionPerformed;
+                _touch2PosAction.performed += OnTouchPosActionPerformed;
+                _touch3PosAction.performed += OnTouchPosActionPerformed;
+                _touch4PosAction.performed += OnTouchPosActionPerformed;
+                _touchPosAction.Enable();
                 _touch0PosAction.Enable();
-                _touch0PressAction.Enable();
+                _touch1PosAction.Enable();
+                _touch2PosAction.Enable();
+                _touch3PosAction.Enable();
+                _touch4PosAction.Enable();
+                OnMoveStarted();
             }
             else
             {
@@ -62,11 +85,19 @@ namespace Madnessnoid
         {
             if (Application.isMobilePlatform)
             {
-                _touch0PressAction.started -= OnTouch0PressStarted;
-                _touch0PressAction.canceled -= OnTouch0PressCanceled;
-                _touch0PosAction.performed -= OnTouch0PosActionPerformed;
+                _touchPosAction.performed -= OnTouchPosActionPerformed;
+                _touch0PosAction.performed -= OnTouchPosActionPerformed;
+                _touch1PosAction.performed -= OnTouchPosActionPerformed;
+                _touch2PosAction.performed -= OnTouchPosActionPerformed;
+                _touch3PosAction.performed -= OnTouchPosActionPerformed;
+                _touch4PosAction.performed -= OnTouchPosActionPerformed;
+                _touchPosAction.Disable();
                 _touch0PosAction.Disable();
-                _touch0PressAction.Disable();
+                _touch1PosAction.Disable();
+                _touch2PosAction.Disable();
+                _touch3PosAction.Disable();
+                _touch4PosAction.Disable();
+                OnMoveCancel();
             }
             else
             {
@@ -75,21 +106,51 @@ namespace Madnessnoid
                 _playerActionsProvider.Attack -= OnAttack;
             }
         }
-
-        private void UpdateTouchDirection(Vector2 pos)
+        private bool IsTouchToTheLeftFromPaddleCenter(Vector2 paddlePos)
         {
-            bool isLeft = pos.x < Screen.width * 0.5f;
-            _touchDirection = isLeft ? -1 : 1;
+            return _touchPosition < (paddlePos.x - _paddleSize.x / 2);
+        }
+        Vector2 GetRendererWorldSize(SpriteRenderer sr)
+        {
+            var sprite = sr.sprite;
+            if (sr.drawMode == SpriteDrawMode.Simple)
+            {
+                Vector2 spriteSize = sprite.rect.size / sprite.pixelsPerUnit;
+                return Vector2.Scale(spriteSize, sr.transform.lossyScale);
+            }
+            else
+            {
+                return Vector2.Scale(sr.size, sr.transform.lossyScale);
+            }
+        }
+        private void UpdateTouchDirection(Vector2 touchPos)
+        {
+            var pos = ScreenToWorld2D(touchPos);
+            _touchPosition = pos.x;
+            _isLeftTouchDirection = IsTouchToTheLeftFromPaddleCenter(_paddleTransform.position);
+            _touchDirection = _isLeftTouchDirection ? -1 : 1;
+        }
+        private Vector3 ScreenToWorld2D(Vector2 touchPos)
+        {
+            var rect = _camera.pixelRect;
+            // поправка на letterbox или другие искажения экрана
+            touchPos.x = Mathf.Clamp(touchPos.x - rect.x, 0, rect.width);
+            touchPos.y = Mathf.Clamp(touchPos.y - rect.y, 0, rect.height);
+            float distance = _camera.orthographic ? Mathf.Abs(_camera.transform.position.z) : 0;
+            return _camera.ScreenToWorldPoint(new Vector3(touchPos.x, touchPos.y, distance));
         }
         private void OnMoveStarted()
         {
-            if (!_ballController.IsPushed)
+            if (!Application.isMobilePlatform && !_ballController.IsPushed)
             {
-                var moveDeltaX = Application.isMobilePlatform ? _touchDirection : _playerActionsProvider.LastMoveDelta.x;
+                var moveDeltaX = _playerActionsProvider.LastMoveDelta.x;
                 var direction = new Vector2(moveDeltaX, 1).normalized;
                 _ballController.Push(direction, _startImpulseStrenth);
             }
-            _moveCts = CancellationTokenSource.CreateLinkedTokenSource(destroyCancellationToken, Application.exitCancellationToken);
+            _moveCts = CancellationTokenSource.CreateLinkedTokenSource(
+                destroyCancellationToken,
+                _paddleTransform.gameObject.GetCancellationTokenOnDestroy(),
+                Application.exitCancellationToken);
             var localMoveCts = _moveCts;
             MoveTaskAsync(_moveCts.Token).ContinueWith(() =>
             {
@@ -100,27 +161,17 @@ namespace Madnessnoid
                 }
             }).Forget();
         }
-        private void OnMoveCancel()
+        private void OnMoveCancel() => _moveCts?.Cancel();
+        private void OnTouchPosActionPerformed(InputAction.CallbackContext ctx)
         {
-            _moveCts?.Cancel();
-        }
-        private void OnTouch0PressStarted(InputAction.CallbackContext ctx)
-        {
-            UpdateTouchDirection(_touch0PosAction.ReadValue<Vector2>());
-            OnMoveStarted();
-        }
-        private void OnTouch0PosActionPerformed(InputAction.CallbackContext ctx)
-        {
-            UpdateTouchDirection(ctx.ReadValue<Vector2>());
-        }
-        private void OnTouch0PressCanceled(InputAction.CallbackContext ctx)
-        {
-            _touchDirection = 0;
-            _moveCts?.Cancel();
+            if (ctx.control.parent is TouchControl touchControl)
+            {
+                var pos = touchControl.position.value;
+                UpdateTouchDirection(pos);
+            }
         }
         private void OnAttack()
         {
-            _logger.Log("Attack works");
             if (!_ballController.IsPushed)
             {
                 _ballController.Push(Vector2.up, _startImpulseStrenth);
@@ -129,16 +180,46 @@ namespace Madnessnoid
 
         private async UniTask MoveTaskAsync(CancellationToken cancellationToken)
         {
-            while (!cancellationToken.IsCancellationRequested)
+            try
             {
-                await UniTask.Yield();
-                var moveDeltaX = Application.isMobilePlatform ? _touchDirection : _playerActionsProvider.LastMoveDelta.x;
-                var moveDelta = moveDeltaX * _moveSpeed * Time.deltaTime;
-                var desiredPositionX = moveDelta + _paddleTransform.position.x;
-                if (desiredPositionX > _minX && desiredPositionX < _maxX)
+                while (!cancellationToken.IsCancellationRequested)
                 {
-                    _paddleTransform.position += moveDelta * Vector3.right;
+                    if (Application.isMobilePlatform)
+                    {
+                        await UniTask.WaitUntil(() => Mathf.Abs(_touchPosition - _paddleTransform.position.x) > float.Epsilon, cancellationToken: cancellationToken);
+                        if (!_ballController.IsPushed)
+                        {
+                            var direction = new Vector2(_touchDirection, 1).normalized;
+                            _ballController.Push(direction, _startImpulseStrenth);
+                        }
+                    }
+                    else
+                    {
+                        await UniTask.Yield();
+                    }
+                    var moveDeltaX = Application.isMobilePlatform ? _touchDirection : _playerActionsProvider.LastMoveDelta.x;
+                    var moveDelta = moveDeltaX * _moveSpeed * Time.deltaTime;
+                    var desiredPositionX = moveDelta + _paddleTransform.position.x;
+                    if (desiredPositionX > _minX && desiredPositionX < _maxX)
+                    {
+                        var newPosition = _paddleTransform.position + moveDelta * Vector3.right;
+                        if (Application.isMobilePlatform)
+                        {
+                            bool isLeftDirection = IsTouchToTheLeftFromPaddleCenter(newPosition);
+                            if (_isLeftTouchDirection != isLeftDirection)
+                            {
+                                newPosition = new Vector3(_touchPosition, newPosition.y, newPosition.z);
+                                _touchDirection = 0;
+                            }
+                        }
+                        _paddleTransform.position = newPosition;
+                    }
                 }
+            }
+            catch (OperationCanceledException) { }
+            catch (Exception ex)
+            {
+                _logger.LogError(nameof(PaddleMovement), ex);
             }
         }
     }

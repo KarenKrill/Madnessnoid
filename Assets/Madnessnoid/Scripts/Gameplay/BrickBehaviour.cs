00995bee1978d43cdb5f73d0ac83c0cb8e513983
KarenKrill <vampart1997@gmail.com>
2025-11-30 04:48:00 +0500

    Added damagable brick behaviour

--- Assets/Madnessnoid/Scripts/Gameplay/BrickBehaviour.cs
+++ Assets/Madnessnoid/Scripts/Gameplay/BrickBehaviour.cs
@@ -1,8 +1,37 @@
+using System;
+
 using UnityEngine;
 
 namespace Madnessnoid
 {
     public class BrickBehaviour : MonoBehaviour
     {
+        public float Health => _health;
+
+        public event Action<BrickBehaviour> Died;
+
+        public void Damage(float value)
+        {
+            _health -= value;
+            if (_health <= float.Epsilon)
+            {
+                try
+                {
+                    OnDied();
+                }
+                finally
+                {
+                    Died?.Invoke(this);
+                }
+            }
+        }
+
+        protected virtual void OnDied()
+        {
+            gameObject.SetActive(false);
+        }
+
+        [SerializeField]
+        private float _health = 1;
     }
 }
